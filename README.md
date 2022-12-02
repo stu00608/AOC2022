442527fc0c3cc96f5516c5b1bacae7f07ac120be
Allen Shen <kuya5220@gmail.com>
2022-12-02 16:10:53 +0800

    Complete day 2 part 2.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 ---
 
-- Day1
+- Day 1
   - Part1 : [⭕](https://github.com/stu00608/AOC2022/blob/master/day1/main.py)
   - Part2 : [⭕](https://github.com/stu00608/AOC2022/blob/master/day1/main.py)
+- Day 2
+  - Part1 : [⭕](https://github.com/stu00608/AOC2022/blob/master/day2/main.py)
+  - Part2 : [⭕](https://github.com/stu00608/AOC2022/blob/master/day2/main.py)
--- day2/main.py
+++ day2/main.py
@@ -8,7 +8,7 @@ mapping = {
 }
 
 
-def rps_checking(pick: int, oppo: int):
+def rps_checking(oppo: int, pick: int):
     """Check if Rock Paper Scissor win, draw or lose.
 
     Parameters
@@ -30,15 +30,40 @@ def rps_checking(pick: int, oppo: int):
     else:
         return 6
 
+def rps_checking_inverse(oppo: int, cond: int):
+    """Return the selection by 
+
+    Parameters
+    ----------
+    oppo : int
+        The opponent selection. 0 for Rock, 1 for Paper, 2 for Scissor.
+    cond : 
+        Condition, 0 for seeking lose, 1 for draw, 2 for win.
+    
+    Return
+    ------
+    int
+        the selection for specified condition, 0 for Rock, 1 for Paper, 2 for Scissor
+    """
+    if cond == 0:
+        return (oppo - 1) % 3
+    elif cond == 1:
+        return oppo
+    else:
+        return (oppo + 1) % 3
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         data_list = f.read().splitlines()
-    total_score = 0
+    total_score_part_1 = 0
+    total_score_part_2 = 0
     for data in data_list:
         oppo, pick = data.split(' ')
-        total_score += rps_checking(mapping[pick], mapping[oppo]) + mapping[pick] + 1
-    print(f"Part 1: {total_score}")
+        oppo, pick = mapping[oppo], mapping[pick]
+        total_score_part_1 += rps_checking(oppo, pick) + pick + 1
+        total_score_part_2 += pick * 3 + rps_checking_inverse(oppo, cond=pick) + 1
+    print(f"Part 1: {total_score_part_1}")
+    print(f"Part 2: {total_score_part_2}")
 
     
     
