4f82e121359dbdd7b761d8d0ad3ddea99728ba07
Allen Shen <kuya5220@gmail.com>
2022-12-03 15:13:21 +0800

    Complete day 3 part 1.

---
+++ day3/main.py
@@ -0,0 +1,28 @@
+def compare(first: str, second: str):
+    """Compare two str, find the one char that appear in both str."""
+    for c in first:
+        if c in second:
+            return c
+    return None
+
+def get_priority(c: str):
+    """Given char return its priority by following rule, a-z:1-26, A-Z:27-52"""
+    assert len(c) == 1
+    if c.islower():
+        return ord(c) - 97 + 1
+    elif c.isupper():
+        return ord(c) - 65 + 27
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data_list = f.read().splitlines()
+
+    priority_list = []
+    for data in data_list:
+        first, second = data[:len(data)//2], data[len(data)//2:]
+        item = compare(first, second)
+        priority = get_priority(item)
+        priority_list.append(priority)
+    
+    print(f"Part 1: {sum(priority_list)}")
+
