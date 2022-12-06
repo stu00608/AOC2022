0d928914aa93beaa1b2823a8c58258fce8edc724
Allen Shen <kuya5220@gmail.com>
2022-12-06 18:25:30 +0800

    Complete day 6 part 2.

--- day6/main.py
+++ day6/main.py
@@ -1,13 +1,25 @@
+def get_marker(data: str, genre: int):
+    """
+
+    Parameters
+    ----------
+    data : str
+        Data stream.
+    genre : int
+        The amount that require to consider as a marker.
+    """
+
+    history = set()
+    for i in range(len(data)-genre):
+        if len(set(data[i:i+genre])) == genre:
+            return i + genre
+
+
 if __name__ == "__main__":
     with open("input.txt") as f:
         data = f.read()
 
-    history = set()
-    for i, c in enumerate(data):
-        if c in history:
-            history = set(c)
-        else:
-            history.add(c)
-        if len(history) == 4:
-            break
-    print(f"Part 1: {i+1}")
+    first_result = get_marker(data, 4)
+    second_result = get_marker(data, 14)
+    print(f"Part 1: {first_result}")
+    print(f"Part 2: {second_result}")
