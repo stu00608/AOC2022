d9d23f8383e43d13aa7ea906716f49a65e74d459
Allen Shen <kuya5220@gmail.com>
2022-12-10 00:58:35 +0800

    Complete day 8 part 2.

--- day8/main.py
+++ day8/main.py
@@ -1,6 +1,32 @@
 import numpy as np
 from typing import List
 
+
+def get_distance(target: int, vision: List[int]):
+    """Given tree target, and its sight from one direction,
+    calculate how many tree it can see from target. Return
+    the amount of visible tree.
+
+    Parameters
+    ----------
+    target : int
+        The center target.
+    vision : List[int]
+        The sight from one direction.
+
+    Return
+    ------
+    int
+        The amount of trees that are visible.
+    """
+    count = 0
+    for item in vision:
+        count += 1
+        if item >= target:
+            break
+    return count
+
+
 def visible_check(target: int, vision: List[int]):
     """Given tree target, and its sight from one direction,
     calculate if the tree is visible from outside by checking
@@ -48,4 +74,14 @@ if __name__ == "__main__":
 
             first_result += 1 if is_visible else 0
 
+    second_result = 0
+    for i in range(len(tree_map)):
+        for j in range(len(tree_map[i])):
+            visible_trees = get_distance(tree_map[i, j], list(reversed(tree_map[i, 0:j]))) *\
+                get_distance(tree_map[i, j], tree_map[i, j+1:size]) *\
+                get_distance(tree_map[i, j], list(reversed(tree_map[0:i, j]))) *\
+                get_distance(tree_map[i, j], tree_map[i+1:size, j])
+            second_result = max(second_result, visible_trees)
+
     print(f"Part 1: {first_result}")
+    print(f"Part 2: {second_result}")
